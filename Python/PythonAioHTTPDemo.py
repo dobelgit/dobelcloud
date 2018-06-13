c6134fb340e5432f6c9a8d61828cd06f1281a6e4
dobelgit <38695857+dobelgit@users.noreply.github.com>
2018-06-13 18:49:41 +0800

    新增aiohttp demo

---
+++ Python/PythonAioHTTPDemo.py
@@ -0,0 +1,28 @@
+import time
+import asyncio
+from aiohttp import ClientSession, TCPConnector, BasicAuth
+
+async def fetch(url, session):
+    proxy_auth = BasicAuth('ud92tos25', 'usgggs25')
+    async with session.get(url=url,
+                           proxy="http://58.215.120.212:9180",
+                           proxy_auth=proxy_auth) as resp:
+        print(await resp.read())
+
+connector = TCPConnector(limit=20)
+session = ClientSession(connector=connector)
+nums = 100
+url = 'http://www.taobao.com/help/getip.php'
+tasks = [fetch(url, session) for x in range(nums)]
+begin = time.time()
+try:
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(asyncio.wait(tasks))
+except:
+        pass
+finally:
+        end = time.time()
+        loop.close()
+        session.close()
+        print('cost', end - begin, 'speed', nums / (end - begin), 'req/s')
+
