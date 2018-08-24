56c86153c7f458f36d6c85667c5bdb1a4004d6df
dobelgit <38695857+dobelgit@users.noreply.github.com>
2018-08-24 11:49:08 +0800

    关闭keepalive

--- Python/PythonAioHttpDemo.py
+++ Python/PythonAioHttpDemo.py
@@ -3,16 +3,18 @@ import asyncio
 from aiohttp import ClientSession, TCPConnector, BasicAuth
 
 async def fetch(url, session):
-    proxy_auth = BasicAuth('ud92tos25', 'usgggs25')
+    proxy_auth = BasicAuth('tstfmsfsfax', '0sfsf0fsf0')
+    headers = {'connection': 'closed'}
     async with session.get(url=url,
-                           proxy="http://http-proxy-sg2.dobel.cn:9180",
-                           proxy_auth=proxy_auth) as resp:
+                           proxy="http://http-proxy-sg1.dobel.cn:9180",
+                           proxy_auth=proxy_auth,
+                           headers=headers) as resp:
         print(await resp.read())
 
-connector = TCPConnector(limit=20)
+connector = TCPConnector(limit=60)
 session = ClientSession(connector=connector)
-nums = 100
-url = 'http://www.taobao.com/help/getip.php'
+nums = 300
+url = 'https://www.taobao.com/help/getip.php'
 tasks = [fetch(url, session) for x in range(nums)]
 begin = time.time()
 try:
@@ -25,4 +27,3 @@ finally:
         loop.close()
         session.close()
         print('cost', end - begin, 'speed', nums / (end - begin), 'req/s')
-
