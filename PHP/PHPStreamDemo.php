cc1e2bb04ef4fc4d2400971a1861c7f61cb9ab44
dobelgit <38695857+dobelgit@users.noreply.github.com>
2019-02-22 14:00:36 +0800

    端口

--- PHP/PHPStreamDemo.php
+++ PHP/PHPStreamDemo.php
@@ -6,7 +6,7 @@
 $auth = base64_encode('账号:密码');//LOGIN:PASSWORD 这里是你的账户名及密码    
 $aContext = array(    
     'http' => array(    
-        'proxy' => 'tcp://域名:9180',//这里设置你要使用的代理服务器域名及端口号    
+        'proxy' => 'tcp://域名:端口',//这里设置你要使用的代理服务器域名及端口号    
         'request_fulluri' => true,    
         'header' => "Proxy-Authorization: Basic $auth",    
     ),    
