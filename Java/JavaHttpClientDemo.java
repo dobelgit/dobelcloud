2b59f2e531ec8486ba810f113d9e392e39ea5db9
dobelgit <38695857+dobelgit@users.noreply.github.com>
2019-07-08 22:41:57 +0800

    解决407的问题

--- Java/JavaHttpClientDemo.java
+++ Java/JavaHttpClientDemo.java
@@ -1,61 +1,73 @@
-
+import javax.net.ssl.SSLContext;
+import org.apache.http.HttpHeaders;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.AuthCache;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.protocol.HttpClientContext;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustSelfSignedStrategy;
+import org.apache.http.impl.auth.BasicScheme;
+import org.apache.http.impl.client.BasicAuthCache;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.ssl.SSLContexts;
 import org.apache.http.util.EntityUtils;
 
-/**
- * A simple example that uses HttpClient to execute an HTTP request
- * over a secure connection tunneled through an authenticating proxy.
- */
 public class JavaHttpClientDemo {
 
-    //目标网址 
-    final static String targetUrl = "https://www.taobao.com/help/getip.php"; 
-    // 代理服务器
-    final static String proxyHost = "域名";
-    final static Integer proxyPort = 端口;
-
-    // 代理隧道验证信息
-    final static String proxyUser = "账号";
-    final static String proxyPass = "密码";
-
-
     public static void main(String[] args) throws Exception {
+        
+        
         CredentialsProvider credsProvider = new BasicCredentialsProvider();
         credsProvider.setCredentials(
-	        AuthScope.ANY,
-                new UsernamePasswordCredentials(proxyUser, proxyPass));
-        CloseableHttpClient httpclient = HttpClients.custom()
-                .setDefaultCredentialsProvider(credsProvider).build();
-        try {
-            HttpHost proxy = new HttpHost(proxyHost, proxyPort);
+                new AuthScope("代理服务器域名", 9180),
+                new UsernamePasswordCredentials("user", "passwd"));
+        
+        try (CloseableHttpClient httpclient = HttpClients.custom()
+                .setDefaultCredentialsProvider(credsProvider)
+                .build())
+        {
+            HttpHost target = new HttpHost("myip.ipip.net", 443, "https");
+            HttpHost proxy = new HttpHost("代理服务器域名", 9180);
+
+            AuthCache authCache = new BasicAuthCache();
+
+            BasicScheme basicAuth = new BasicScheme();
+            basicAuth.processChallenge(
+                    new BasicHeader(HttpHeaders.PROXY_AUTHENTICATE,
+                                    "Basic realm=\"Crawlera\""));
+            authCache.put(proxy, basicAuth);
+
+            HttpClientContext ctx = HttpClientContext.create();
+            ctx.setAuthCache(authCache);
 
             RequestConfig config = RequestConfig.custom()
                 .setProxy(proxy)
                 .build();
-            HttpGet httpget = new HttpGet(targetUrl);
+            
+            HttpGet httpget = new HttpGet("/");
             httpget.setConfig(config);
 
-            System.out.println("Executing request  to " + httpget + " via " + proxy);
+            System.out.println("Executing request " + httpget.getRequestLine() +
+                " to " + target + " via " + proxy)PROXY_AUTHENTICATE = ";
 
-            CloseableHttpResponse response = httpclient.execute(httpget);
-            try {
+            try (CloseableHttpResponse response = httpclient.execute(
+                target, httpget, ctx))
+            {
                 System.out.println("----------------------------------------");
                 System.out.println(response.getStatusLine());
+                System.out.println("----------------------------------------");
                 System.out.println(EntityUtils.toString(response.getEntity()));
-            } finally {
-                response.close();
+                EntityUtils.consume(response.getEntity());
             }
-        } finally {
-            httpclient.close();
         }
     }
+
 }
